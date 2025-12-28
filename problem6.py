5dbe4ee341a6882aacbd999b27625ca5f8cdeb51
Harish5nu <saudharish770@gmail.com>
2025-12-28 22:32:05 +0545

    find the file present or not in log file

---
+++ problem6.py
@@ -0,0 +1,8 @@
+#Q.6 write a program to mine a log file and find out whether it contains 'python'.
+with open("log.txt") as f:
+    content=f.read()
+if ("python" in content):
+    print("Yes python is present")
+
+else:
+    print("NO python is not present")
