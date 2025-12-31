809b7d99aabfc84639b593d4cf624924e9009635
Harish5nu <saudharish770@gmail.com>
2025-12-31 21:57:38 +0545

    file problems are compledted

---
+++ problem9.py
@@ -0,0 +1,12 @@
+#Q. write a program to find out whether a file is identical & matches the content of antoher file.
+with open("this.txt") as f:
+    content1 = f.read()
+
+with open("this_copy.txt") as f:
+    content2 = f.read()
+
+if(content1 == content2):
+    print("Yes these files are identical")
+
+else: 
+    print("No these files are not identical")
